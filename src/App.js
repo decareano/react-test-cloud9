b4f19faf3199e8fd4667e89f2d6593ec715e2d1c
EC2 Default User <ec2-user@ip-10-0-0-145.ec2.internal>
2021-07-21 23:40:38 +0000

    new branch. added Hard coded data and Table.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import logo from './logo.svg';
 import './App.css';
+import Table from './Table'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello, React man!</h1>
+      <div className="container">
+        <Table />
       </div>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
 
 export default App
