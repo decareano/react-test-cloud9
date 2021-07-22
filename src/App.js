00d8b45c653430e2646d57f4dca73c23286cd450
Marcelo Gobelli <marcelo@marcelos-mbp.gateway.sonic.net>
2021-07-21 22:18:09 -0700

    working Auth0 with login button plus logout button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import logo from './logo.svg';
+//import ReactDOM from 'react-dom'
+//import logo from './logo.svg';
 import './App.css';
 import Table from './Table'
+import LoginButton from './LoginButton'
+import LogoutButton from './LogoutButton'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Table />
+        <LoginButton />
+        <LogoutButton />
       </div>
     )
   }
