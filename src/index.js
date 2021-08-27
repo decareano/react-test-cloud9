472f0e18d392e891a1ccdc899cbca63b205a450a
Marcelo Gobelli <marcelo@marcelos-mbp.gateway.sonic.net>
2021-08-26 21:38:27 -0700

    added simple Auth0 with login and logout-WIP so far

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Auth0Provider } from "@auth0/auth0-react";
+
 
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-dqd-oq6b.us.auth0.com"
+    clientId="EC7ypdJS4x1v9q8qRQfibqeZskmPN9SX"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );
