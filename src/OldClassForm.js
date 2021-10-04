187d7e6ab68416a6e897ab60bb4870061dc4ec06
EC2 Default User <ec2-user@ip-10-0-101-230.ec2.internal>
2021-10-04 13:45:53 +0000

    attempting to change Form.js to functional component

--- src/OldClassForm.js
+++ src/OldClassForm.js
@@ -1,50 +1,50 @@
-import React, { useState } from 'react'
+
+import React, { Component } from 'react'
 import { Container, Row, Col, Navbar } from 'react-bootstrap'
 
-function Form() {
+class Form extends Component {
     
-    const [date, setDate] = useState('');
-    const [systolic, setSystolic] = useState('');
-    const [diastolic, setDiastolic] = useState('');
-    const [pulse, setPulse] = useState('');
-    const [change, setChange] = useState('');
-    
+        initialState = {
+            date: '',
+            systolic: '',
+            diastolic: '',
+            pulse: '',
+        }
+        state = this.initialState
+        
     
 
 
-    const handleDate = (event) => {
-        setDate(event.target.value)
 
-    }
-    
-    const handleSystolic = (event) => {
-        setSystolic(event.target.value)
+    handleChange = (event) => {
+        const {name, value} = event.target
 
-    }
-    
-    const handleDiastolic = (event) => {
-        setDiastolic(event.target.value)
-
-    }
-    
-    const handlePulse = (event) => {
-        setPulse(event.target.value)
-
-    }
-    
-   
-    
-    const handleChange = (event) => {
-        const { change, value } = event.target
-        setChange({
-            [change]: value
+        this.setState({
+            [name]: value
         })
+
     }
+
     
-    const handleSubmit = (event) => {
-        //const { name, value } = event.target
-    }
+
+    submitForm = () => {
+        // edit in class: using ...this.state, id: Date.now() adds an ID or uniqueness to the item. then we pass the newState as props to handleSubmit method
+        const newState = {
+          ...this.state,
+          id: Date.now()
+        }
+      
+        this.props.handleSubmit(newState);
+        this.setState(this.initialState);
+    } 
+
     
+    
+    
+
+    render() {
+        const { date, systolic, diastolic, pulse} = this.state;
+        
 
         return (
             <>
@@ -70,33 +70,33 @@ function Form() {
                     id="date"
                     //use defaultValue instead of value
                     value={date}
-                    onChange={handleDate} />
+                    onChange={this.handleChange} />
                 <label htmlFor="systolic">Systolic Pressure</label>
                 <input
                     type="text"
                     name="systolic"
                     id="systolic"
                     value={systolic}
-                    onChange={handleSystolic} />
+                    onChange={this.handleChange} />
                 <label htmlFor="diastolic">Diastolic Pressure</label>
                 <input
                     type="text"
                     name="diastolic"
                     id="diastolic"
                     value={diastolic}
-                    onChange={handleDiastolic} />
+                    onChange={this.handleChange} />
                 <label htmlFor="pulse">Pulse</label>
                 <input
                     type="text"
                     name="pulse"
                     id="pulse"
                     value={pulse}
-                    onChange={handlePulse} />
+                    onChange={this.handleChange} />
 
                 <input className="btn btn-primary"
                     type="button"
                     value="Submit"
-                    onClick={handleSubmit} />  
+                    onClick={this.submitForm} />  
 
                
             </>
@@ -109,7 +109,7 @@ function Form() {
 
             
         )
-    
+    }
 }
 
 
